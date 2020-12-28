e652f96fce532987d7670fd584a2495fff690c7c
CYZ <1822550890@qq.com>
2020-12-28 16:02:16 +0000

    对象

--- src/day01/PersonTest.java
+++ src/day01/PersonTest.java
@@ -4,7 +4,28 @@ public class PersonTest {
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-
+		Person p = new Person("mike",18,true);
+		p.eat();
+		p.talk();
 	}
 
+}
+class Person{
+	String name;
+	int age;
+	boolean isMale;
+	
+	public Person(String name, int age, boolean isMale) {
+		this.name = name;
+		this.age = age;
+		this.isMale = isMale;
+	}
+	
+	public void eat() {
+		System.out.println("进食");
+	}
+	
+	public void talk() {
+		System.out.println("说话");
+	}
 }
