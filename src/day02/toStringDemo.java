da5837f2c138f08c5a6c61cd584f8b9f7ad4b8a8
CYZ <1822550890@qq.com>
2020-12-31 15:58:26 +0000

    测试完毕

--- src/day02/toStringDemo.java
+++ src/day02/toStringDemo.java
@@ -4,7 +4,12 @@ public class toStringDemo {
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-
+		toStringDemo t = new toStringDemo();
+		//当输出一个对象的引用时，自动调用toString方法
+		System.out.println(t);
 	}
-
+	
+	public String toString() {
+		return "重写的方法";
+	}
 }
