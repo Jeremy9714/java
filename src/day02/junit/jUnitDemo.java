32000a0283fdb1b82c729342c7df1326b4994465
CYZ <1822550890@qq.com>
2020-12-31 16:33:35 +0000

    单元测试

--- src/day02/junit/jUnitDemo.java
+++ src/day02/junit/jUnitDemo.java
@@ -1,10 +1,26 @@
 package day02.junit;
 
+import java.sql.Date;
+
+import org.junit.Test;
+
 public class jUnitDemo {
-
-	public static void main(String[] args) {
-		// TODO 自动生成的方法存根
-
+	int num = 10;
+	@Test
+	public void testEquals() {
+		String s1 = "AA";
+		String s2 = "BB";
+		System.out.println(s1.equals(s2));
+		
+//		Object o = new String("GG");
+//		Date d = (Date) o;
+		
+		//单元测试方法中可以直接调用属性，不需要初始化对象
+		System.out.println(num);
 	}
-
+	
+	@Test
+	public void testToString() {
+		System.out.println(num+"");
+	}
 }
