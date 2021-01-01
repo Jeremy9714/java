805d1e92a28d5922859b8b75fb9b54b83a9893aa
CYZ <1822550890@qq.com>
2021-01-01 21:07:26 +0000

    代理模式实例1

--- src/day03/test6/ProxyTest.java
+++ src/day03/test6/ProxyTest.java
@@ -1,10 +1,47 @@
 package day03.test6;
-
+/*
+ * 代理模式
+ */
 public class ProxyTest {
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-
+		Server server = new Server();
+		ProxyServer proxyServer = new ProxyServer(server);
+		proxyServer.browse();
 	}
 
 }
+
+interface Network{
+	public void browse();
+}
+
+//被代理类
+class Server implements Network{
+
+	@Override
+	public void browse() {
+		System.out.println("服务器访问网络");
+	}
+}
+//代理类
+class ProxyServer implements Network{
+
+	private Network work;
+	
+	public ProxyServer(Network work) {
+		this.work = work;
+	}
+	
+	public void check() {
+		System.out.println("联网前的检查工作");
+	}
+	
+	@Override
+	public void browse() {
+		check();
+		work.browse();
+	}
+}
+
