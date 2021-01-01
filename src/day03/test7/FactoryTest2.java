2ca32992c53ebc77cbbb03e5f467415909077317
CYZ <1822550890@qq.com>
2021-01-01 22:04:39 +0000

    test

--- src/day03/test7/FactoryTest2.java
+++ src/day03/test7/FactoryTest2.java
@@ -5,8 +5,41 @@ package day03.test7;
 public class FactoryTest2 {
 
 	public static void main(String[] args) {
-		// TODO 自动生成的方法存根
-
+		
+		Car car1 = new BMWFactory().getCar();
+		Car car2 = new BenzFactory().getCar();
+		car1.run();
+		car2.run();
 	}
+	
+}
+interface Car{
+	void run();
+}
+class BMW implements Car{
+	public void run() {
+		System.out.println("BMW is running");
+	}
+}
+class Benz implements Car{
+	public void run() {
+		System.out.println("Benz is running");
+	}
+}
+//工厂接口
+interface Factory{
+	Car getCar();
+}
+class BMWFactory implements Factory{
+	@Override
+	public Car getCar() {
+		return new BMW();
+	}
+}
+class BenzFactory implements Factory{
 
+	@Override
+	public Car getCar() {
+		return new Benz();
+	}
 }
