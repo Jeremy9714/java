663cfe44d45b498956dda7c651db66f12ed0f485
CYZ <1822550890@qq.com>
2021-01-01 12:06:12 +0000

    静态和非静态代码块

--- src/day03/test1/BlockTest.java
+++ src/day03/test1/BlockTest.java
@@ -4,7 +4,29 @@ public class BlockTest {
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
+		Block block = new Block();
+		Block block2 = new Block();
+		System.out.println(Block.desc);
+	}
+
+}
+
+class Block {
+	String name;
+	int age;
+	static String desc;
+
+	public Block() {
 
 	}
 
+	{
+		System.out.println("代码块执行");
+		name = "A";
+		age = 18;
+	}
+	static {
+		System.out.println("静态代码块执行");
+		desc = "静态字符串";
+	}
 }
