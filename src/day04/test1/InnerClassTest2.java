5005174b367e2217417efc01f4a4ba02932bc162
CYZ <1822550890@qq.com>
2021-01-02 13:06:39 +0000

    局部内部类的使用

--- src/day04/test1/InnerClassTest2.java
+++ src/day04/test1/InnerClassTest2.java
@@ -3,8 +3,40 @@ package day04.test1;
 public class InnerClassTest2 {
 
 	public static void main(String[] args) {
-		// TODO 自动生成的方法存根
 
+
+	}
+
+}
+
+class Test {
+
+	public Comparable getComparable() {
+
+//		// 方式一
+//		// 创建一个实现了Comparable接口的局部内部类
+//		class MyComparable implements Comparable {
+//
+//			@Override
+//			public int compareTo(Object o) {
+//				// TODO 自动生成的方法存根
+//				return 0;
+//			}
+//
+//		}
+//		return new MyComparable();
+		
+		//方式二
+		//匿名类匿名对象
+		return new Comparable() {
+
+			@Override
+			public int compareTo(Object o) {
+				// TODO 自动生成的方法存根
+				return 0;
+			}
+			
+		};
 	}
 
 }
