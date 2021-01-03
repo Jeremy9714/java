ee055d716e5ee67ff2e08c13e44c237edc144bc9
CYZ <1822550890@qq.com>
2021-01-03 13:06:26 +0000

    设计师和架构师类

---
+++ src/day05/myproject/domain/Architect.java
@@ -0,0 +1,23 @@
+package day05.myproject.domain;
+
+public class Architect extends Designer {
+	private int stock;
+
+	public Architect() {
+		// TODO 自动生成的构造函数存根
+	}
+
+	public Architect(int id, String name, int age, double salary, Equipment equipment, double bonus, int stock) {
+		super(id, name, age, salary, equipment, bonus);
+		this.stock = stock;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+}
