ee89b60d4adc0b093f1eb7eff62d4d697db7fec1
CYZ <1822550890@qq.com>
2021-01-03 14:12:07 +0000

    自定义异常类

---
+++ src/day05/myproject/services/TeamException.java
@@ -0,0 +1,12 @@
+package day05.myproject.services;
+
+public class TeamException extends Exception {
+	static final long serialVersionUID = -2223397190245766939L;
+
+	public TeamException() {
+	}
+
+	public TeamException(String msg) {
+		super(msg);
+	}
+}
