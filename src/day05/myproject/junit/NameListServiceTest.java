537a0e9d76a5895bf9853ace07da71b7a72fe077
CYZ <1822550890@qq.com>
2021-01-03 16:40:39 +0000

    debug

--- src/day05/myproject/junit/NameListServiceTest.java
+++ src/day05/myproject/junit/NameListServiceTest.java
@@ -1,4 +1,5 @@
 package day05.myproject.junit;
+
 import org.junit.Test;
 
 import day05.myproject.domain.*;
@@ -7,17 +8,24 @@ import day05.myproject.services.*;
 public class NameListServiceTest {
 
 	@Test
-	public void test1(){
-		System.out.println("id\t名字\t年龄\t薪水\t设备\t\t奖金\t股份");
+	public void test1() {
+		System.out.println("ID\t姓名\t年龄\t工资\t职位\t状态\t奖金\t股份\t领用设备");
+
+		NameListService n = new NameListService();
+		Employee[] employees = n.getAllEmployees();
+		for (Employee e : employees) {
+			System.out.println(e);
+		}
+	}
+	
+	@Test
+	public void test2() {
 		try {
 			NameListService n = new NameListService();
-			Employee[] employees = n.getAllEmployees();
-			for(Employee e:employees) {
-				System.out.println(e);
-			}
-			n.getEmployee(0);
-		} catch (TeamException e) {
-			//e.printStackTrace();
+			Employee employee = n.getEmployee(0);
+			System.out.println("ID\t姓名\t年龄\t工资\t职位\t状态\t奖金\t股份\t领用设备");
+			System.out.println(employee);
+		}catch(TeamException e) {
 			System.out.println(e.getMessage());
 		}
 	}
